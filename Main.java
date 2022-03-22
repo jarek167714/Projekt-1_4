09ea68c67a82b329e493781daa4b72c999277e62
jarek167714 <101186879+jarek167714@users.noreply.github.com>
2022-03-22 12:23:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) 
+  {
+    Scanner nazwa = new Scanner(System.in);
+    int wybor;
+    double a ,b;
+    System.out.println("Co chcesz policzyć? \n1. Potęga \n2. Suma pierwiastków \n3. Max \n4. Dzielenie");
+    wybor = nazwa.nextInt();
+
+    switch(wybor)
+      {
+        case 1:
+          {
+            System.out.println("Liczba do potęgi: \n Podaj a i b");
+            a = nazwa.nextDouble();
+            b = nazwa.nextDouble();
+            double power = Math.pow(a ,b);
+            System.out.println("a ^ b = "+power);
+            break;
+          }
+      }
+  }
+  
+}
